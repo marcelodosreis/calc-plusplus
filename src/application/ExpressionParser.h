15cee8c5e727e57d31c72167d6442b2be42e8bdf
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-01-16 20:58:25 -0300

    refactor: expression parser

--- src/application/ExpressionParser.h
+++ src/application/ExpressionParser.h
@@ -12,54 +12,11 @@
 class ExpressionParser {
 public:
   IExpression *parse(const std::string &input) const {
-    std::istringstream stream(input);
-    double firstOperand;
-    char operatorSymbol;
-    double secondOperand;
-
-    IExpression *result = new Number(firstOperand);
-    int openParenthesesCount = 0;
-    int closeParenthesesCount = 0;
-
-    for (char ch : input) {
-        if (ch == '(') {
-            openParenthesesCount++;
-        } else if (ch == ')') {
-            closeParenthesesCount++;
-        }
+      for (size_t i = 0; i < input.length(); ++i) {
+        std::cout << input[i] << std::endl;
     }
 
-    std::cout << "Parênteses de abertura '(': " << openParenthesesCount << std::endl;
-    std::cout << "Parênteses de fechamento ')': " << closeParenthesesCount << std::endl;
-
-    if (openParenthesesCount != closeParenthesesCount) {
-        throw std::invalid_argument("Número de parênteses não correspondentes.");
-    }
-
-    if (!(stream >> firstOperand)) {
-      throw std::invalid_argument("Formato inválido. Esperando um número.");
-    }
-
-    while (stream >> operatorSymbol) {
-      if (operatorSymbol != '+' && operatorSymbol != '-') {
-        throw std::invalid_argument(
-            "Operador inválido. Apenas + e - são suportados.");
-      }
-
-      if (!(stream >> secondOperand)) {
-        throw std::invalid_argument(
-            "Formato inválido. Esperando um número após o operador.");
-      }
-
-      Number *rightOperand = new Number(secondOperand);
-
-      if (operatorSymbol == '+') {
-        result = new Addition(result, rightOperand);
-      } else if (operatorSymbol == '-') {
-        result = new Subtraction(result, rightOperand);
-      }
-    }
-
+    IExpression *result = new Number(2);
     return result;
   }
 };
