5e702894ecd40fc9cbe44edff299692ce33e33ed
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-01-16 21:27:13 -0300

    feat: add validation for parenthesis

--- src/application/ExpressionParser.h
+++ src/application/ExpressionParser.h
@@ -4,26 +4,40 @@
 #include "../domain/entities/Addition.h"
 #include "../domain/entities/Number.h"
 #include "../domain/entities/Subtraction.h"
-#include <cctype> // Para std::isdigit
+#include <cctype>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 class ExpressionParser {
 public:
   IExpression *parse(const std::string &input) const {
 
     std::unordered_set<char> validChars = {'(', ')', '+', '-', '*', '/'};
+    std::vector<char> array;
+    int parenthesisCount = 0;
 
     for (char c : input) {
+      if (c == ')') {
+        parenthesisCount--;
+      }
+
       if (std::isdigit(c) || validChars.count(c)) {
-        std::cout << c << ": Caractere Valido" << std::endl;
-      } else {
-        std::cout << c << ": Caractere Invalido" << std::endl;
+        if (c == '(') {
+          parenthesisCount++;
+        }
+        array.push_back(c);
       }
     }
+
+    if (parenthesisCount != 0) {
+      std::cout << "Parenteses Estão faltando" << std::endl;
+    }
+
+    std::string str(array.begin(), array.end());
 
     IExpression *result = new Number(2);
     return result;
