e05f037b09bca96750e05b274acb83a83c634b32
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-02-19 21:42:50 -0300

    feat: analizing numbers inside parenthesis

--- src/application/ExpressionParser.h
+++ src/application/ExpressionParser.h
@@ -12,36 +12,46 @@
 #include <unordered_set>
 #include <vector>
 
-class ExpressionParser {
+class ExpressionParser
+{
 public:
-  IExpression *parse(const std::string &input) const {
+  IExpression *parse(const std::string &input) const
+  {
     std::unordered_set<char> validChars = {'(', ')', '+', '-', '*', '/'};
     std::vector<char> array;
     int parenthesisCount = 0;
     bool isLastOperator = false;
     std::vector<std::vector<int>> parenthesisBlocks;
 
-    for (int i = 0; i < input.size(); i++) {
+    for (int i = 0; i < input.size(); i++)
+    {
       char c = input[i];
 
-      if (c == '+' || c == '-' || c == '*' || c == '/') {
-        if (isLastOperator) {
-          std::cout << "Erro: Problema de Duplo Sinal: " << c << std::endl;
+      if (c == '+' || c == '-' || c == '*' || c == '/')
+      {
+        if (isLastOperator)
+        {
+          std::cout << "Error: Double operator issue: " << c << std::endl;
           return nullptr;
         }
 
         isLastOperator = true;
-      } else {
+      }
+      else
+      {
         isLastOperator = false;
       }
 
-      if (std::isdigit(c) || validChars.count(c)) {
-        if (c == ')') {
+      if (std::isdigit(c) || validChars.count(c))
+      {
+        if (c == ')')
+        {
           parenthesisCount--;
           parenthesisBlocks.back().push_back(i);
         }
-        
-        if (c == '(') {
+
+        if (c == '(')
+        {
           parenthesisCount++;
           parenthesisBlocks.push_back({i});
         }
@@ -49,14 +59,32 @@ public:
       }
     };
 
-    if (parenthesisCount != 0) {
-      std::cout << "Erro: Parênteses desbalanceados." << std::endl;
+    if (parenthesisCount != 0)
+    {
+      std::cout << "Error: Unbalanced parentheses." << std::endl;
       return nullptr;
     }
-    
-    IExpression *result1 = new Number(+3);
-    IExpression *result2 = new Number(-2);
-    IExpression* result = new Addition(result1, result2);
+
+    for (size_t i = 0; i < parenthesisBlocks.size(); ++i)
+    {
+      for (size_t j = 0; j < parenthesisBlocks[i].size() - 1; ++j)
+      {
+        int a = parenthesisBlocks[i][j];
+        int b = parenthesisBlocks[i][j + 1];
+
+        int start = std::min(a, b);
+        int end = std::max(a, b);
+
+        int quantity = end - start - 1;
+
+        std::cout << "Quantity of numbers inside parenthesis: " << quantity << " ";
+      }
+      std::cout << std::endl;
+    }
+
+    IExpression *number1 = new Number(+3);
+    IExpression *number2 = new Number(-2);
+    IExpression *result = new Addition(number1, number2);
     return result;
   }
 };
