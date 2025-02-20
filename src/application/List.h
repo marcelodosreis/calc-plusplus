30d1b7e9eeaea0daaa7842f04114043f270c3eeb
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-02-19 21:58:48 -0300

    refactor: creating base of a list to learn perfomance validation in sequence interpreter

---
+++ src/application/List.h
@@ -0,0 +1,15 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+
+class List
+{
+public:
+    void teste(const std::string data)
+    {
+        std::cout << "User Input: " << data << std::endl;
+    }
+};
+
+#endif // LIST_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <string>
 #include "presentation/CLIInputHandler.h"
 #include "application/ExpressionParser.h"
+#include "application/List.h"
 
 int main() {
     CLIInputHandler cliInputHandler;
     ExpressionParser expressionParser;
+    List list;
 
     cliInputHandler.clearScreen();
     cliInputHandler.showMenu();
@@ -16,18 +18,19 @@ int main() {
             cliInputHandler.showOutput("Encerrando o programa.", BasicCLI::Color::RED);
             break;
         }
+        list.teste(userInput);
 
-        try {
-            IExpression* expression = expressionParser.parse(userInput);
-            double result = expression->evaluate();
+        // try {
+        //     IExpression* expression = expressionParser.parse(userInput);
+        //     double result = expression->evaluate();
 
 
-            cliInputHandler.showOutput("Resultado: " + std::to_string(result), BasicCLI::Color::GREEN);
+        //     cliInputHandler.showOutput("Resultado: " + std::to_string(result), BasicCLI::Color::GREEN);
             
-            delete expression;
-        } catch (const std::exception& e) {
-            cliInputHandler.showOutput(std::string("Erro: ") + e.what(), BasicCLI::Color::RED);
-        }
+        //     delete expression;
+        // } catch (const std::exception& e) {
+        //     cliInputHandler.showOutput(std::string("Erro: ") + e.what(), BasicCLI::Color::RED);
+        // }
     }
 
     cliInputHandler.clearScreen();
