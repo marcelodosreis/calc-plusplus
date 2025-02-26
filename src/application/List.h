5e8751389eb0acb9a299f8c9fbe463df15a20f41
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-02-25 21:55:12 -0300

    feat: testing a basic simple list

--- src/application/List.h
+++ src/application/List.h
@@ -8,7 +8,14 @@ class List
 public:
     void teste(const std::string data)
     {
-        std::cout << "User Input: " << data << std::endl;
+        // std::cout << "User Input: " << data << std::endl;
+        std::vector<int> lista = {1, 2, 3, 4, 5}; // Lista de números
+
+        lista.push_back(6); // Adiciona um número no final
+        lista.pop_back();   // Remove o último número
+    
+        std::cout << "Primeiro elemento: " << lista[0] << std::endl;
+        std::cout << "Último elemento: " << lista.back() << std::endl;
     }
 };
 
